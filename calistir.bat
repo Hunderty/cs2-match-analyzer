fe52ec5357a0b157318430cc884a196006e1c2ae
UmutKadiroglu <umutkadiroglu1643@gmail.com>
2026-04-03 21:37:48 +0300

    proje temizlendi, masaustu uygulamasi tamamlandi

--- calistir.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo CS2 Mac Analizci basliyor...
-python -m streamlit run app.py --server.maxUploadSize 1000
-pause
--- main.py
+++
@@ -1,8 +0,0 @@
-from analyzer import veri_yukle, istatistik_goster, harita_analizi, grafik_ciz, kazanma_tahmini, harita_kazanma
-
-df = veri_yukle()
-istatistik_goster(df)
-harita_analizi(df)
-kazanma_tahmini(df)
-harita_kazanma(df)
-grafik_ciz(df)
